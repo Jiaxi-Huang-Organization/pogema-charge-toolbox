3036e51c6cd1663e29f871a47bbe3e0be28566e3
jiaxi <1761475917@qq.com>
2026-03-08 16:28:43 +0800

    (fix): remove follower feat

--- eval_example.py
+++ eval_example.py
@@ -1,6 +1,5 @@
 from pogema_toolbox.evaluator import evaluation
 from pogema import BatchAStarAgent
-from pogema import Follower
 
 from pathlib import Path
 
@@ -15,14 +14,20 @@ BASE_PATH = Path('config_examples')
 
 def main():
     ToolboxRegistry.setup_logger(level='INFO')
-    ToolboxRegistry.register_env('Pogema-v0', create_env_base, Environment)
+    env_cfg_name = 'Pogema-v0'
+    ToolboxRegistry.register_env(env_cfg_name, create_env_base, Environment)
     ToolboxRegistry.register_algorithm('A*', BatchAStarAgent)
-
+    #ToolboxRegistry.register_algorithm('Follower', FollowerInference, FollowerInferenceConfig, follower_preprocessor)
     folder_names = [
         'logo',
+        #'pathfinding'
     ]
 
     for folder in folder_names:
+        #maps_path = BASE_PATH / folder / "maps.yaml"
+        #with open(maps_path, 'r') as f:
+        #    maps = yaml.safe_load(f)
+        #ToolboxRegistry.register_maps(maps)
         config_path = BASE_PATH / folder / f"{Path(folder).name}.yaml"
         eval_dir = BASE_PATH / folder
 
